e04295377eabdb2ae61b32d42a7d2683e6a60b3e
JoaoKuzinor <joaopedrokuzinorlima@hotmail.com>
2026-05-17 00:52:35 -0300

    Primeiro commit

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+# from flask import Blueprint, jsonify
+
+# main = Blueprint("main", __name__)
+
+
+# @main.route("/status", methods=["GET"])
+# def health_check():
+#     return jsonify({"status": "ok"})
